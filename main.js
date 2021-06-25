f00570460156e0fc57396e4d6ba94c1ff5e7395a
MarioCastigliano <mario.castigliano@outlook.it>
2021-06-25 23:22:31 +0200

    lang

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 console.log('Initialized. \n Lang=it');
 
-const clickLang = function (lang) {
+const clickLang = function () {
     if( !$(this).hasClass("active-l") ){
         $(".it").toggleClass("active-l");
         $(".en").toggleClass("active-l");
-        console.log('lang=' + lang);
     }
 }
 
-$(".it").click( clickLang("it") );
-$(".en").click( clickLang("en") );
+$(".it").click( clickLang );
+$(".en").click( clickLang );
