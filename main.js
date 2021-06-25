7308cd01c9d63d8b40bbf173b58637ea5fe81e90
MarioCastigliano <mario.castigliano@outlook.it>
2021-06-25 23:18:12 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('Initialized. \n Lang=it');
+
+const clickLang = function () {
+    if( !$(this).hasClass("active-l") ){
+        $(".it").toggleClass("active-l");
+        $(".en").toggleClass("active-l");
+    }
+}
+
+$(".it").click( clickLang("it") );
+$(".en").click( clickLang("en") );
